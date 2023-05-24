3509c5ce66a4aec262a0a431a2bd78c98239165d
Denis Rogov <kpotuw@gmail.com>
2023-05-24 10:54:19 +0300

    Lesson 91. Frist Spring REST App. Init commit.

---
+++ src/main/java/ru/springcourse/FirstRestApp/repositories/PeopleRepository.java
@@ -0,0 +1,9 @@
+package ru.springcourse.FirstRestApp.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.springcourse.FirstRestApp.models.Person;
+
+@Repository
+public interface PeopleRepository extends JpaRepository<Person, Integer> {
+}
